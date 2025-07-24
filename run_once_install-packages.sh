be2a7eae090d8d43599040841923482139ed384f
Igor Sirotin <igor.sirotin.1012@gmail.com>
2025-07-24 12:54:32 +0100

    feat: alt-tab

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,6 +3,7 @@
 # Install brew packages
 brew install lsd tree vivid stats
 brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask alt-tab
 
 # Configure Vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
