1614fe3766d13be7a466d06e96e9006522b59192
Igor Sirotin <igor.sirotin.1012@gmail.com>
2025-08-01 20:25:34 +0100

    feat: autocompletions

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,6 +5,10 @@ brew install lsd tree vivid stats
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask alt-tab
 
+brew install zsh-completions
+chmod go-w '/opt/homebrew/share'
+chmod -R go-w '/opt/homebrew/share/zsh'
+
 # Configure Vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
