636954c57d5612d2a12d18f193d3783d8ab566f8
Igor Sirotin <sirotin@status.im>
2025-09-26 09:37:44 +0100

    feat: add nix, delete qlty

--- dot_zshrc.d/qlty.sh
+++
@@ -1,6 +0,0 @@
-# qlty completions
-[ -s "/opt/homebrew/share/zsh/site-functions/_qlty" ] && source "/opt/homebrew/share/zsh/site-functions/_qlty"
-
-# qlty
-export QLTY_INSTALL="$HOME/.qlty"
-export PATH="$QLTY_INSTALL/bin:$PATH"
