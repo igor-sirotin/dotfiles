ed290c3931a47ded337a6220d1fc10ebf6309205
Igor Sirotin <igor.sirotin.1012@gmail.com>
2026-01-19 08:22:42 +0000

    feat: bin paths

--- dot_zshrc.d/paths.sh
+++ dot_zshrc.d/paths.sh
@@ -1,3 +1,5 @@
+export PATH="/Users/sirotin/bin:$PATH"
 export PATH="/opt/homebrew/bin/:$PATH"
 export PATH="/opt/homebrew/sbin/:$PATH"
 export PATH="/Users/sirotin/go/bin:$PATH"
+export PATH="/Users/sirotin/.local/bin:$PATH"
