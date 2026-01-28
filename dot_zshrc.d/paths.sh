e2c0597be7fbc95b0d00b05ea8560bd64ef23644
Igor Sirotin <igor.sirotin.1012@gmail.com>
2026-01-28 14:30:43 +0000

    feat: nix in path

--- dot_zshrc.d/paths.sh
+++ dot_zshrc.d/paths.sh
@@ -3,3 +3,9 @@ export PATH="/opt/homebrew/bin/:$PATH"
 export PATH="/opt/homebrew/sbin/:$PATH"
 export PATH="/Users/sirotin/go/bin:$PATH"
 export PATH="/Users/sirotin/.local/bin:$PATH"
+
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
